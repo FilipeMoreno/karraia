4089e7760de3a06a8f0c64c62b21bd3fafa37ac7
Filipe Moreno <y8.filipe@gmail.com>
2024-06-24 14:50:28 -0300

    feat: alterar exibição da playlist quando está pausada

--- src/app/playlist/page.tsx
+++ src/app/playlist/page.tsx
@@ -261,18 +261,6 @@ export default function PlaylistIndex() {
 		return <PlaylistLogin />
 	}
 
-	if (!playlistConfig || !playlistConfig.isPlaying) {
-		return (
-			<main className="flex min-h-screen flex-col items-center gap-8 p-4">
-				<LogoComponent />
-				<div className="flex w-[60%] flex-col items-center justify-center gap-2 rounded-lg bg-white p-4">
-					<FaExclamationTriangle className="animate-pulse text-6xl text-red-500" />
-					<p className="text-xl">A playlist está pausada.</p>
-				</div>
-			</main>
-		)
-	}
-
 	const orderedPlaylist = filteredMusicas.sort((a, b) => {
 		if (a.tocada === b.tocada) {
 			return b.addedAt - a.addedAt
@@ -287,68 +275,78 @@ export default function PlaylistIndex() {
 				<div className="flex w-full items-center justify-center md:hidden">
 					<AddMusicComponent />
 				</div>
-				{currentMusic && (
-					<Card className="w-full">
-						<CardHeader>
-							<CardTitle>Tocando agora</CardTitle>
-						</CardHeader>
-						{currentMusic && (
-							<>
-								<PlaylistTocandoAgora
-									id={currentMusic.id}
-									musica={currentMusic.title}
-									url={currentMusic.url}
-									imagem={currentMusic.thumbnail}
-									onEnd={handleEndCurrentMusic}
-									isAdmin={isAdmin}
-									onRemove={() => handleRemoveMusic(currentMusic.id)}
-								/>
+				{!playlistConfig ||
+					(!playlistConfig.isPlaying && (
+						<div className="flex w-full flex-col items-center justify-center gap-2 rounded-lg bg-white p-4">
+							<FaExclamationTriangle className="animate-pulse text-6xl text-red-500" />
+							<p className="text-xl">A playlist está pausada.</p>
+						</div>
+					)) || (
+						<>
+							{currentMusic && (
+								<Card className="w-full">
+									<CardHeader>
+										<CardTitle>Tocando agora</CardTitle>
+									</CardHeader>
+									{currentMusic && (
+										<>
+											<PlaylistTocandoAgora
+												id={currentMusic.id}
+												musica={currentMusic.title}
+												url={currentMusic.url}
+												imagem={currentMusic.thumbnail}
+												onEnd={handleEndCurrentMusic}
+												isAdmin={isAdmin}
+												onRemove={() => handleRemoveMusic(currentMusic.id)}
+											/>
 
-								<div className="flex flex-col items-center justify-normal gap-2 bg-zinc-100 p-4 lg:flex-row lg:justify-between">
-									<div className="flex flex-row items-center justify-center gap-2">
-										<Image
-											src={currentMusic.addedByPhoto}
-											height={40}
-											width={40}
-											alt={currentMusic.addedByName}
-											className="rounded-full"
-										/>
-										<div className="flex flex-col">
-											<p className="text-xs">Adicionado por:</p>
-											<p>{currentMusic.addedByName}</p>
-										</div>
-									</div>
-									<div className="flex items-center justify-end gap-4">
-										{isAdmin && (
-											<Button
-												variant={'destructive'}
-												onClick={handleSkipCurrentMusic}
-											>
-												<FaForward className="mr-2" /> Pular Música
-											</Button>
-										)}
-										<Button
-											className={userSkipVote ? 'text-[#7FD7EB]' : ''}
-											onClick={handleVoteToSkip}
-										>
-											{userSkipVote ? (
-												<>
-													<FaCheck className="mr-2" />
-													{` Pular ${skipVotes}/${requiredVotes}`}
-												</>
-											) : (
-												<>
-													<FaForward className="mr-2" />
-													{` Pular ${skipVotes}/${requiredVotes}`}
-												</>
-											)}
-										</Button>
-									</div>
-								</div>
-							</>
-						)}
-					</Card>
-				)}
+											<div className="flex flex-col items-center justify-normal gap-2 bg-zinc-100 p-4 lg:flex-row lg:justify-between">
+												<div className="flex flex-row items-center justify-center gap-2">
+													<Image
+														src={currentMusic.addedByPhoto}
+														height={40}
+														width={40}
+														alt={currentMusic.addedByName}
+														className="rounded-full"
+													/>
+													<div className="flex flex-col">
+														<p className="text-xs">Adicionado por:</p>
+														<p>{currentMusic.addedByName}</p>
+													</div>
+												</div>
+												<div className="flex items-center justify-end gap-4">
+													{isAdmin && (
+														<Button
+															variant={'destructive'}
+															onClick={handleSkipCurrentMusic}
+														>
+															<FaForward className="mr-2" /> Pular Música
+														</Button>
+													)}
+													<Button
+														className={userSkipVote ? 'text-[#7FD7EB]' : ''}
+														onClick={handleVoteToSkip}
+													>
+														{userSkipVote ? (
+															<>
+																<FaCheck className="mr-2" />
+																{` Pular ${skipVotes}/${requiredVotes}`}
+															</>
+														) : (
+															<>
+																<FaForward className="mr-2" />
+																{` Pular ${skipVotes}/${requiredVotes}`}
+															</>
+														)}
+													</Button>
+												</div>
+											</div>
+										</>
+									)}
+								</Card>
+							)}
+						</>
+					)}
 
 				<div className="flex w-full flex-col gap-4 lg:flex-row">
 					<Card className="w-full lg:w-[45%]">
