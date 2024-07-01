5730fdecfd9fb6bc9d81779f611d8e522a97dcde
Filipe Moreno <y8.filipe@gmail.com>
2024-07-01 10:20:47 -0300

    Obrigado!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,99 +1,11 @@
 'use client'
 
 import FooterCard from '@/components/footer-card'
-import Loading from '@/components/loading'
 import LogoComponent from '@/components/logo'
-import { Button } from '@/components/ui/button'
-import {
-	Card,
-	CardContent,
-	CardFooter,
-	CardHeader,
-	CardTitle,
-} from '@/components/ui/card'
-import { userAuthContext } from '@/context/AuthContext'
-import { googleLogin } from '@/lib/authService'
-import { database } from '@/lib/firebaseService'
-import { child, get, ref } from 'firebase/database'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import Image from 'next/image'
-import Link from 'next/link'
-import { useRouter } from 'next/navigation'
-import { useEffect, useState } from 'react'
-import { FaGoogle, FaHeart } from 'react-icons/fa'
-import { toast } from 'sonner'
 
 export default function Home() {
-	const { userAuth } = userAuthContext()
-	const [carregando, setCarregando] = useState(true)
-	const route = useRouter()
-
-	if (userAuth) {
-		get(child(ref(database), `confirmados/${userAuth?.uid}`))
-			.then((snapshot) => {
-				if (snapshot.exists()) {
-					route.push('/confirmar/confirmado')
-				} else {
-					route.push('/confirmar')
-				}
-			})
-			.catch((error) => {
-				toast.error('Erro ao verificar confirmação de presença', {
-					description: error,
-				})
-				console.error('Erro ao verificar confirmação de presença: ', error)
-			})
-			.finally(() => {
-				setCarregando(false)
-			})
-	}
-
-	const [time, setTime] = useState({
-		days: 0,
-		hours: 0,
-		minutes: 0,
-		seconds: 0,
-	})
-
-	const [isCountdownInitialized, setIsCountdownInitialized] = useState(false)
-
-	useEffect(() => {
-		const interval = setInterval(() => {
-			const eventDate = new Date(Date.UTC(2024, 5, 29, 18, 0, 0))
-			eventDate.setHours(eventDate.getHours())
-
-			const now = new Date()
-			const nowUTC = new Date(
-				Date.UTC(
-					now.getFullYear(),
-					now.getMonth(),
-					now.getDate(),
-					now.getHours(),
-					now.getMinutes(),
-					now.getSeconds(),
-				),
-			)
-			const diff = eventDate.getTime() - nowUTC.getTime()
-
-			const days = Math.floor(diff / (1000 * 60 * 60 * 24))
-			const hours = Math.floor(
-				(diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
-			)
-			const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60))
-			const seconds = Math.floor((diff % (1000 * 60)) / 1000)
-
-			setTime({ days, hours, minutes, seconds })
-			setIsCountdownInitialized(true)
-		}, 1000)
-
-		return () => clearInterval(interval)
-	}, [])
-
-	if (carregando) {
-		if (userAuth) {
-			return <Loading />
-		}
-	}
-
 	return (
 		<>
 			<main className="flex min-h-screen flex-col items-center gap-8 bg-fj bg-no-repeat p-4">
@@ -110,70 +22,13 @@ export default function Home() {
 						</CardTitle>
 					</CardHeader>
 					<CardContent className="flex flex-col gap-4">
-						<div className="w-full rounded-lg bg-zinc-100 p-12 text-center lg:p-4">
-							{isCountdownInitialized && time.days === 0 && time.hours < -24 ? (
-								<div className="mt-4 text-center">
-									<h2 className="font-bold text-xl">É hoje!</h2>
-								</div>
-							) : isCountdownInitialized &&
-								time.days === 0 &&
-								time.hours === 0 &&
-								time.minutes === 0 &&
-								time.seconds === 0 ? (
-								<div className="mt-4 text-center">
-									<h2 className="font-bold text-xl">O evento começou!</h2>
-								</div>
-							) : (
-								<>
-									<p className="mb-4 font-bold font-caveat text-4xl">Faltam</p>
-									<div className="flex flex-row items-center justify-center gap-2 font-bold font-jura text-xl lg:gap-4 lg:text-2xl">
-										{time.days > 0 && (
-											<span>
-												{time.days}
-												<br />
-												<small>dias</small>
-											</span>
-										)}
-										{time.hours > 0 && (
-											<span>
-												{time.hours}
-												<br />
-												<small>horas</small>
-											</span>
-										)}
-										{time.minutes > 0 && (
-											<span>
-												{time.minutes}
-												<br />
-												<small>minutos</small>
-											</span>
-										)}
-										<span>
-											{time.seconds}
-											<br />
-											<small>segundos</small>
-										</span>
-									</div>
-								</>
-							)}
-						</div>
-						<div className="rounded-lg bg-zinc-100 p-4">
-							<h1 className="mb-4 text-center font-bold font-caveat text-4xl">
-								Confirme sua presença!
-							</h1>
-							<p className="text-center">Não vai ficar de fora, né?</p>
-							<p className="text-center">
-								Faça login com o Google clicando no botão abaixo para confirmar
-								sua presença.
-							</p>
-						</div>
+						<b className="text-center">Muito Obrigada!</b>
+						<p className="text-justify">
+							Muito obrigada por terem ido. Esse dia foi muito feliz e especial
+							para mim! Espero que vocês também tenham gostado! Te espero no
+							próximo KArraiá!
+						</p>
 					</CardContent>
-
-					<CardFooter className="flex flex-col items-center">
-						<Button className="w-full" onClick={googleLogin}>
-							<FaGoogle className="mr-2" /> Entrar com Google
-						</Button>
-					</CardFooter>
 				</Card>
 				<FooterCard />
 			</main>
